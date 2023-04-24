7ac555c02c0ed4e4685065177e713fbb2a32dbb7
1bauer <alexander@1bauer.de>
2023-04-24 19:42:21 +0200

    feat: added container component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import { FakestoreapiService } from '../services/fakestoreapi/fakestoreapi.service';
+import { Product } from '../services/fakestoreapi/models/product';
+import { RandomuserapiService } from '../services/randomuserapi/randomuserapi.service';
+import { Observable } from 'rxjs';
+import { User } from '../services/randomuserapi/models/user';
 
 @Component({
   selector: 'app-root',
@@ -6,4 +11,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+
+  products: Observable<Product[]>;
+  productsColumns: string[] = ["id", "title", "category", "price"];
+  users: Observable<User[]>;
+  usersColumns: string[] = ["uid","username", "first_name", "last_name"]
+
+  constructor(fakestore: FakestoreapiService, randomuserapi: RandomuserapiService) {
+    this.products = fakestore.getProducts();
+    this.users = randomuserapi.getUsers();
+
+  }
 }
